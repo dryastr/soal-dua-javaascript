c3136e76f4112719381b4f7a60dbdc94aa0e32bb
dryastr <darayastore01@gmail.com>
2024-06-05 21:38:35 +0700

    pus

---
+++ index-dua.js
@@ -0,0 +1,34 @@
+class Animal {
+    constructor(name, age, isMammal) {
+      this.name = name;
+      this.age = age;
+      this.isMammal = isMammal;
+    }
+  }
+  
+  class Rabbit extends Animal {
+    constructor(name, age) {
+      super(name, age, true);
+    }
+  
+    eat() {
+      return `${this.name} sedang makan!`;
+    }
+  }
+  
+  class Eagle extends Animal {
+    constructor(name, age) {
+      super(name, age, false);
+    }
+  
+    fly() {
+      return `${this.name} sedang terbang!`;
+    }
+  }
+  
+  const myRabbit = new Rabbit('Labi', 2);
+  console.log(myRabbit.eat());
+  
+  const myEagle = new Eagle('Elo', 4);
+  console.log(myEagle.fly());
+  
